5c9d8d3219bd5b6287c6cc9223c0696a7b32699d
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2022-08-04 02:00:02 +0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 cd ~ && \
 sudo apt-get update && \
-sudo apt-get install pipenv v4l2loopback-utils ffmpeg fonts-liberation git python3-pip vim -y && \
+sudo apt-get install v4l2loopback-utils ffmpeg fonts-liberation git python3-pip vim -y && \
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && \
 sudo dpkg -i google-chrome-stable_current_amd64.deb && \
 sudo modprobe v4l2loopback card_label="FakeCam" exclusive_caps=1
